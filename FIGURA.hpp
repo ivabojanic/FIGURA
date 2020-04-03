7226aad9ee7b2d4eb5bd540e90c098623e0f0fb7
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-03 20:09:04 +0200

    Update FIGURA.hpp

--- FIGURA.hpp
+++ FIGURA.hpp
@@ -1,1 +1,36 @@
 
+#ifndef FIGURA_H_INCLUDED
+#define FIGURA_H_INCLUDED
+
+#include <cmath>
+
+class Figura
+{
+    protected:
+        double a;
+        double b;
+    public:
+        double getOPravougaonika()
+        {
+            double O = 2*(a+b);
+            return O;
+        }
+        double getOElipse()const
+        {
+            double O = 3.14*(3*(a + b)-sqrt((3*a + b)*(a + 3*b)));
+            return O;
+        }
+        double getPPravougaonika()const
+        {
+            double P = a*b;
+            return P;
+        }
+        double getPElipse()const
+        {
+            double P = a*b*3.14;
+            return P;
+        }
+;
+
+
+#endif // FIGURA_H_INCLUDED
