e95a49559e27f515decc62f058978a12d001543b
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-03 20:10:10 +0200

    Update KRUG.hpp

--- KRUG.hpp
+++ KRUG.hpp
@@ -1,1 +1,15 @@
+#ifndef KRUG_HPP_INCLUDED
+#define KRUG_HPP_INCLUDED
+
+#include "VALJAK.hpp"
+
+class Krug : public Elipsa
+{
+    public:
+        Krug() : Elipsa(2,2){getObim();getPovrsina();}
+        Krug(double aa) : Elipsa(aa, aa) {getObim();getPovrsina();}
+        Krug(const Krug &k) : Elipsa(k.a, k.b) {getObim();getPovrsina();}
+};
+
+#endif // KRUG_HPP_INCLUDED
 
