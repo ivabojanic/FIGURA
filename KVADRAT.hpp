dcc1593b47de0bf043a7e419e6dfe7ae63c97897
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-03 20:10:30 +0200

    Update KVADRAT.hpp

--- KVADRAT.hpp
+++ KVADRAT.hpp
@@ -1,1 +1,15 @@
 
+#ifndef KVADRAT_HPP_INCLUDED
+#define KVADRAT_HPP_INCLUDED
+
+#include "PRAVOGUAONIK.hpp"
+
+class Kvadrat : public Pravougaonik
+{
+    public:
+        Kvadrat() : Pravougaonik(2,2){getObimm();getPovrsinaa();}
+        Kvadrat(double aa) : Pravougaonik(aa, aa) {getObimm();getPovrsinaa();}
+        Kvadrat(const Kvadrat &kv) : Pravougaonik(kv.a, kv.b) {getObimm();getPovrsinaa();}
+};
+
+#endif // KVADRAT_HPP_INCLUDED
