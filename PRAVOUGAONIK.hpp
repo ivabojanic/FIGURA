efaf04e6d8d17d762d65aa4e964778142baec9f1
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-08 17:23:25 +0200

    Update PRAVOUGAONIK.hpp

--- PRAVOUGAONIK.hpp
+++ PRAVOUGAONIK.hpp
@@ -3,18 +3,34 @@
 #define PRAVOGUAONIK_HPP_INCLUDED
 
 #include "FIGURA.h"
+#include <cmath>
 
 class Pravougaonik : public Figura
 {
-    public:
+private:
+    double a;
+    double b;
+public:
+    Pravougaonik(){a = 10.00; b = 15.00;}
+    Pravougaonik(double aa, double bb){a = aa; b = bb;}
+    Pravougaonik(const Pravougaonik &P){ a = P.a; b = P.b;}
 
-        double getObimm()const{return getOPravougaonika();}
-        double getPovrsinaa()const{return getPPravougaonika();}
+    double getA()const {return a;}
+    double getB()const {return b;}
 
-        Pravougaonik() : Figura(2,3){getObimm();getPovrsinaa();}
-        Pravougaonik(double aa, double bb) : Figura(aa, bb) {getObimm();getPovrsinaa();}
-        Pravougaonik(const Pravougaonik &p) : Figura(p.a, p.b) {getObimm();getPovrsinaa();}
+    void setA(double A){a = A;}
+    void setB(double B){b = B;}
 
+    double Povrsina()
+    {
+        double P = a*b;
+        return P;
+    }
+    double Obim()
+    {
+        double O = 2*(a+b);
+        return O;
+    }
 };
 
 
